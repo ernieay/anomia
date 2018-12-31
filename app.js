c6fd1fc42287717e864cb8418a1a9bb43f5675b7
ernieay <e.au-yeung@live.com.au>
2018-12-31 15:58:06 +1100

    functionality done

--- app.js
+++ app.js
@@ -1,24 +1,49 @@
 // Define the `phonecatApp` module
 var codenameApp = angular.module('codenameApp', []);
-
+const NUM_SYMBOLS = 7;
 // Define the `GameController` controller on the `phonecatApp` module
 codenameApp.controller('GameController', function GameController($scope, $http) {
   $scope.words = [];
   $scope.pile = [];
+  $scope.score = 0;
   
-  // $scope.refresh = function(){
-	//   $http({
-  //     method: 'GET',
-  //     url: 'https://l7plker6t7.execute-api.ap-southeast-2.amazonaws.com/prod/getwords'
-	//   }).then(function successCallback(response) {
-  //     $scope.words = response.data;
-	//   }, function errorCallback(response) {
-  //     // called asynchronously if an error occurs
-  //     // or server returns response with an error status.
-	//   });
-  // };  
+  $scope.refresh = function(){
+	  $http({
+      method: 'GET',
+      url: 'https://s3-ap-southeast-2.amazonaws.com/ernieanomia/dictionary.json'
+	  }).then(function successCallback(response) {
+      $scope.words = response.data;
+	  }, function errorCallback(response) {
+      // called asynchronously if an error occurs
+      // or server returns response with an error status.
+	  });
+  };  
 
+  $scope.addCard = function(){
+    let newWord = $scope.words[Math.floor(Math.random() * $scope.words.length)];
+    let newSymbol = Math.floor(Math.random() * NUM_SYMBOLS);
+    let card = {
+      word: newWord,
+      symbol: newSymbol
+    };
+    $scope.pile.push(card);
+  };
 
+  $scope.removeCard = function(){
+    if ($scope.pile.length > 0) {
+      $scope.pile.pop();
+    }
+  };
+
+  $scope.addScore = function(){
+    $scope.score = $scope.score + 1;
+  };
+
+  $scope.getImgSrc = function(){
+    return `${$scope.pile[$scope.pile.length - 1].symbol}.png`;
+  }
+
+  $scope.refresh();
   
   
 });
